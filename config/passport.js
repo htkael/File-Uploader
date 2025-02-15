4aa7e8360498994e2b18ef8ef483a052c3e55af4
htkael <htkael@gmail.com>
2025-02-15 13:56:03 -0600

    Debugging

--- config/passport.js
+++ config/passport.js
@@ -11,21 +11,27 @@ const passportConfig = (app) => {
   passport.use(
     new LocalStrategy(async (username, password, done) => {
       try {
+        console.log("Login attempt for username:", username);
         const user = await prisma.user.findUnique({
           where: {
             username: username,
           },
         });
+
+        console.log("User found:", !!user);
+
         if (!user) {
           return done(null, false, { message: "Incorrect username" });
         }
         const match = await bcrypt.compare(password, user.password);
+        console.log("Password match:", match);
 
         if (!match) {
           return done(null, false, { message: "Incorrect password" });
         }
         return done(null, user);
       } catch (err) {
+        console.error("Strategy error:", err);
         return done(err);
       }
     })
